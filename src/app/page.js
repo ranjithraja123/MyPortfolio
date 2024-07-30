313bcfe0823c203856597eed5bb880f120cc46e6
ranjithraja123 <52457937+ranjithraja123@users.noreply.github.com>
2024-07-30 23:05:31 +0530

    Padding updated

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import EmailSection from "./components/EmailSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-black container mx-auto px-12 py-4">
+    <main className="flex min-h-screen flex-col bg-black container mx-auto px-3 py-4">
     <Navbar />
       {/* <h1 className="text-black">Port Website</h1> */}
       <div className="container mt-24 mx-auto px-1 py-2">
